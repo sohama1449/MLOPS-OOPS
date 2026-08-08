30cd6b430ee584d71150a6bcb1fc32bf243e96a1
sohama1449 <sohamkachare1449@gmail.com>
2026-08-08 11:32:32 +0530

    clas and obj basics done

---
+++ oops1.py
@@ -0,0 +1,22 @@
+class Employee:
+    # special funcion/methods/ magic methos(constructor)/dunder method
+    def __init__(self):
+        print("starts the constructor")
+        self.id = 123
+        self.designation = "Software Engineer"
+        self.salary = 50000
+        print("ends the constructor")
+        
+    def travel(self, destination):
+        print("Travelling method is called")
+        print(f"Travelling to {destination}")
+            
+
+#create an obj/ instance of class Employee
+
+sam = Employee()
+
+#print(sam.id)        
+sam.travel("Kolhapur")
+
+print(type(sam))
---
+++ rough.py
@@ -0,0 +1,10 @@
+lst = [1,2,3]
+my_str = "Hello, World!"
+my_int = 133
+
+print(type(lst))
+print(type(my_str))
+print(type(my_int))
+
+lst.clear()
+print(lst)
