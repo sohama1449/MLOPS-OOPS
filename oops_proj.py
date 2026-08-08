6a6ea5b3e538d49dee1ea880cfe025fd062130e8
sohama1449 <sohamkachare1449@gmail.com>
2026-08-08 11:53:21 +0530

    signup and signin added

--- oops_proj.py
+++ oops_proj.py
@@ -14,9 +14,9 @@ class Chatbook:
                            \n 5. press 5 to Logout""")    
         
         if user_input == "1":
-            pass
+            self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input == "4":
@@ -24,6 +24,33 @@ class Chatbook:
         else:
             exit()
             
+    def signup(self):
+        email = input("Enter your email: ")
+        password = input("Enter your password: ")
+        
+        self.user_name = email
+        self.password = password
+        
+        print(f"Signup successful! Welcome, {self.user_name}!")
+        print("/n")
+        self.menu()
+        
+    def signin(self):
+        if self.user_name == "" and self.password == "":
+            print("No user found. Please sign up first.")
+        else:
+            input_email = input("Enter your email: ")
+            input_password = input("Enter your password: ")
             
+            if input_email == self.user_name and input_password == self.password:
+                print(f"Signin successful! Welcome back, {self.user_name}!")
+                self.logged_in = True
+            else:
+                print("Invalid email or password. Please try again.")
+        
+        print("/n")
+        self.menu()
+                       
+               
 obj = Chatbook()            
             
