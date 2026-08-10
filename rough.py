1a54f87ead9b779ec16412217989828a938e4194
sohama1449 <sohamkachare1449@gmail.com>
2026-08-10 10:53:41 +0530

    commit on main branch

--- rough.py
+++ rough.py
@@ -1,18 +1,18 @@
-lst = [1,2,3]
-my_str = "Hello, World!"
-my_int = 133
-
-print(type(lst))
-print(type(my_str))
-print(type(my_int))
-
-lst.clear()
-print(lst)
-
-from oops_proj import Chatbook # oops concept behind this line
-
-user1 = Chatbook()
-user2 = Chatbook()
+#lst = [1,2,3]
+#my_str = "Hello, World!"
+#my_int = 133
+#
+#print(type(lst))
+#print(type(my_str))
+#print(type(my_int))
+#
+#lst.clear()
+#print(lst)
+#
+#from oops_proj import Chatbook # oops concept behind this line
+#
+#user1 = Chatbook()
+#user2 = Chatbook()
 #
 #nt(user1.get_name())
 #r1.set_name("AgentX")
@@ -26,3 +26,6 @@ user2 = Chatbook()
 ## print(user3.id)
 ## user4= Chatbook()
 #print(user4.id)
+
+
+print(a=5)
